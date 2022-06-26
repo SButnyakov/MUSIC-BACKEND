6611dba61ea3968186c9cfe5721a898c25239445
Sergey <sergey.butnyakov@gmail.com>
2022-06-26 20:13:43 +0300

    [CONFIG] <INIT>

---
+++ src/main/java/ru/butnyakov/music/config/MvcConfig.java
@@ -0,0 +1,34 @@
+package ru.butnyakov.music.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.sql.DataSource;
+
+
+@ComponentScan("ru.butnyakov.music")
+@EnableJpaRepositories("ru.butnyakov.music.repositories")
+@EnableWebMvc
+@PropertySource("classpath:application.properties")
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    Environment environment;
+
+    @Bean
+    public DataSource getDataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName(environment.getProperty("spring.datasource.driver-class-name"));
+        dataSourceBuilder.url(environment.getProperty("spring.datasource.url"));
+        dataSourceBuilder.username(environment.getProperty("spring.datasource.username"));
+        dataSourceBuilder.password(environment.getProperty("spring.datasource.password"));
+        return dataSourceBuilder.build();
+    }
+}
